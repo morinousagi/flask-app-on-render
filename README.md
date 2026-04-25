ba38c920247bd7e958b9094eda0d060cdcc187a6
Jasmine <senzhitu@gmail.com>
2026-04-25 05:43:10 +0000

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# flask-app-on-render
+# Deploy Flask App on Render
+
+The **app** serves as a main menu that directs user to different applications.
+
+## Files
+```
+/
+├── app.py              # Flask backend
+├── requirements.txt    # List of dependencies
+├── render.yaml         # Optional: Blueprint for Render (or configure via UI)
+└── templates/
+    └── index.html      # Your mobile GUI
+```
---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # Define URLs
+    links = {
+        "rag": "https://huggingface.co/spaces/morinousagi/multiagent-rag-ai-governance",
+        "chatbot": "https://web-chat.global.assistant.watson.cloud.ibm.com/preview.html?region=jp-tok&integrationID=78ea9a0d-0044-4ebe-b6b2-7a8584e8a080&serviceInstanceID=5b98a954-462a-4f7c-b268-f13bf625c0bd",
+        "vit": "https://huggingface.co/spaces/morinousagi/pytorch-vit-defect-detect"
+    }
+    return render_template('index.html', links=links)
+
+if __name__ == '__main__':
+    app.run(debug=True)
