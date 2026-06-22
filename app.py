75f2a853fb161518121979e37175f569ed00fec5
Jasmine <senzhitu@gmail.com>
2026-06-22 11:49:25 +0800

    Update links

--- app.py
+++ app.py
@@ -9,8 +9,9 @@ def index():
     links = {
         "rag": "https://huggingface.co/spaces/morinousagi/multiagent-rag-ai-governance",
         "intel": "https://huggingface.co/spaces/morinousagi/nlp-intelligence-analyzer",
+        "vlm": "https://huggingface.co/spaces/morinousagi/video-incident-understanding-agent",
         "chatbot": "https://web-chat.global.assistant.watson.cloud.ibm.com/preview.html?region=jp-tok&integrationID=78ea9a0d-0044-4ebe-b6b2-7a8584e8a080&serviceInstanceID=5b98a954-462a-4f7c-b268-f13bf625c0bd",
-        "vit": "https://huggingface.co/spaces/morinousagi/pytorch-vit-defect-detect"
+        #"vit": "https://huggingface.co/spaces/morinousagi/pytorch-vit-defect-detect"
     }
     return render_template('index.html', links=links)
 
